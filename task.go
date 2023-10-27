265ceaf73bbc8996718ed5b09b52cbf2bb55c55c
246859 <2633565580@qq.com>
2023-10-27 15:54:30 +0800

    feat(woker): supported timeout worker
    1.supported timeout worker
    2.fix panic when sending to closed chan

--- task.go
+++ task.go
@@ -20,9 +20,12 @@ type Task interface {
 
 const DefaultJobSize = 10
 
-var ErrorTaskStillRunning = errors.New("task still running")
-
-var ErrJobPanic = errors.New("job panic")
+var (
+	ErrorTaskStillRunning = errors.New("task still running")
+	ErrWorkerPanic        = errors.New("worker panic")
+	ErrNilWorker          = errors.New("nil worker is not allowed")
+	ErrWorkerTimeout      = errors.New("worker timeout")
+)
 
 var t Task = &task{}
 
@@ -48,17 +51,49 @@ func newTask(ctx context.Context, cancelFunc context.CancelFunc) *task {
 	return &task{
 		ctx:     ctx,
 		cancel:  cancelFunc,
-		jobs:    make([]Worker, 0, DefaultJobSize),
+		workers: make([]Worker, 0, DefaultJobSize),
 		wait:    sync.WaitGroup{},
 		mutex:   sync.Mutex{},
 		running: false,
 	}
 }
 
-type Worker func(ctx context.Context) error
+type WorkerFn func(ctx context.Context) error
+
+func NewWorker(fn WorkerFn) Worker {
+	return Worker{worker: fn}
+}
+
+func NewTimeoutWorker(fn WorkerFn, timeout time.Duration) Worker {
+	return Worker{worker: fn, timeout: timeout}
+}
+
+type Worker struct {
+	worker  func(ctx context.Context) error
+	timeout time.Duration
+}
 
 func (w Worker) work(ctx context.Context) error {
-	return w(ctx)
+	if w.timeout != 0 {
+		return w.workTimeout(ctx)
+	}
+	return w.worker(ctx)
+}
+
+func (w Worker) workTimeout(ctx context.Context) error {
+	done := make(chan struct{})
+	var err error
+	go func() {
+		err = w.worker(ctx)
+		done <- struct{}{}
+		close(done)
+	}()
+	select {
+	case <-time.After(w.timeout):
+		return ErrWorkerTimeout
+	case <-done:
+		return err
+	}
 }
 
 type task struct {
@@ -69,8 +104,8 @@ type task struct {
 	ctx    context.Context
 	cancel context.CancelFunc
 
-	jobs    []Worker
-	waitJos []Worker
+	workers []Worker
+	waiter  []Worker
 	onPanic func(err any)
 
 	err     error
@@ -103,8 +138,8 @@ func (t *task) Clear() error {
 	}
 
 	t.wait.Wait()
-	t.jobs = append(t.waitJos)
-	t.waitJos = t.waitJos[:0]
+	t.workers = append(t.waiter)
+	t.waiter = t.waiter[:0]
 
 	return nil
 }
@@ -117,39 +152,46 @@ func (t *task) Add(j ...Worker) {
 	t.mutex.Lock()
 	defer t.mutex.Unlock()
 	if !t.running {
-		t.jobs = append(t.jobs, j...)
+		t.workers = append(t.workers, j...)
 	} else {
-		t.waitJos = append(t.waitJos, j...)
+		t.waiter = append(t.waiter, j...)
 	}
 }
 
 // Run workers until all of them finished
-// if finished all jobs successfully, it will return nil
+// if finished all workers successfully, it will return nil
 // else if ctx was cancelled, it will return context.Canceled
 // else it will return the error that worker returned
 func (t *task) Run() error {
 	if !t.running {
 
-		t.setRunning(true)
-		t.wait.Add(len(t.jobs))
+		// iterate over a snapshot of a certain moment
+		for _, job := range t.workers {
+			if job.worker == nil {
+				return ErrNilWorker
+			}
+		}
 
-		for _, job := range t.jobs {
+		t.setRunning(true)
+		t.wait.Add(len(t.workers))
+
+		for _, job := range t.workers {
 			go func(w Worker) {
 				// wait count--
 				defer t.wait.Done()
 
 				taskCh := make(chan struct{})
-				defer close(taskCh)
 
 				// worker goroutine
 				go func() {
+					defer close(taskCh)
 					// panic recovery
 					defer func() {
 						if err := recover(); err != nil {
 							if t.onPanic != nil {
 								t.onPanic(err)
 							}
-							t.Cancel(errors2.Wrap(ErrJobPanic, fmt.Sprintf("%+v", err)))
+							t.Cancel(errors2.Wrap(ErrWorkerPanic, fmt.Sprintf("%+v", err)))
 						}
 					}()
 
@@ -175,7 +217,7 @@ func (t *task) Run() error {
 		t.Clear()
 	}
 
-	// if all jobs finished normally, t.err should be nil
+	// if all workers finished normally, t.err should be nil
 	if t.err == nil {
 		// may be ctx was canceled
 		t.err = t.ctx.Err()
--- task_test.go
+++ task_test.go
@@ -8,141 +8,43 @@ import (
 	"time"
 )
 
-func TestTask(t *testing.T) {
+func TestWorker(t *testing.T) {
 	ctx := context.Background()
 
-	task, cancel := New(ctx)
-	defer cancel(nil)
-
-	w1 := func(ctx context.Context) error {
-		for i := 0; i < 100; i++ {
-			time.Sleep(time.Millisecond)
-			fmt.Println("w1", 2*i+1)
-		}
-		panic("unexpected panic")
+	// #1 timeout worker
+	w1 := NewTimeoutWorker(func(ctx context.Context) error {
+		time.Sleep(time.Second)
 		return nil
-	}
+	}, time.Millisecond*500)
+	err1 := w1.work(ctx)
+	assert.Equal(t, ErrWorkerTimeout, err1)
 
-	task.Add(w1, w1, w1, w1, w1)
-	actual := task.Run()
-
-	assert.Equal(t, nil, actual)
-}
-
-func TestCancel(t *testing.T) {
-	ctx := context.Background()
-
-	task, cancel := New(ctx)
-	cancel(nil)
-
-	w1 := func(ctx context.Context) error {
-		for i := 0; i < 100; i++ {
-			time.Sleep(time.Millisecond)
-			fmt.Println("w1", 2*i+1)
-		}
-		panic("unexpected panic")
+	// #2 no timeout worker
+	w2 := NewWorker(func(ctx context.Context) error {
+		time.Sleep(time.Second)
 		return nil
-	}
-
-	task.Add(w1, w1, w1, w1, w1)
-	actual := task.Run()
-
-	assert.Equal(t, context.Canceled, actual)
-}
-
-func TestPanic(t *testing.T) {
-	ctx := context.Background()
-
-	task, cancel := New(ctx)
-	defer cancel(nil)
-
-	w1 := func(ctx context.Context) error {
-		for i := 0; i < 100; i++ {
-			time.Sleep(time.Millisecond)
-			fmt.Println("w1", 2*i+1)
-		}
-		panic("unexpected panic")
-		return nil
-	}
-
-	task.Add(w1, w1, w1, w1, w1)
-	actual := task.Run()
-
-	assert.NotEqual(t, nil, actual)
+	})
+	err2 := w2.work(ctx)
+	assert.Equal(t, nil, err2)
 }
 
 func TestTimeout(t *testing.T) {
 	ctx := context.Background()
 
-	task, cancel := WithTimeout(ctx, time.Second)
-	defer cancel(nil)
+	tt, cancel := WithTimeout(ctx, time.Millisecond*100)
+	defer cancel(context.Canceled)
 
-	w1 := func(ctx context.Context) error {
-		for i := 0; i < 100; i++ {
-			time.Sleep(time.Millisecond)
-			fmt.Println("w1", 2*i+1)
-		}
+	w1 := NewWorker(func(ctx context.Context) error {
+		time.Sleep(time.Second)
+		fmt.Println("w1")
 		return nil
+	})
+
+	for i := 0; i < 100; i++ {
+		tt.Add(w1)
 	}
 
-	task.Add(w1, w1, w1, w1, w1)
-	actual := task.Run()
-
-	assert.Equal(t, context.DeadlineExceeded, actual)
-}
-
-func TestWithDeadLine(t *testing.T) {
-	ctx := context.Background()
-
-	task, cancel := WithDeadLine(ctx, time.Now().Add(time.Second))
-	defer cancel(nil)
-
-	w1 := func(ctx context.Context) error {
-		for i := 0; i < 100; i++ {
-			time.Sleep(time.Millisecond)
-			fmt.Println("w1", 2*i+1)
-		}
-		return nil
-	}
-
-	task.Add(w1, w1, w1, w1, w1)
-	actual := task.Run()
-
-	assert.Equal(t, context.DeadlineExceeded, actual)
-}
-
-func TestClear(t *testing.T) {
-	ctx := context.Background()
-
-	task, cancel := New(ctx)
-	defer cancel(nil)
-
-	w1 := func(ctx context.Context) error {
-		for i := 0; i < 100; i++ {
-			time.Sleep(time.Millisecond)
-			fmt.Println("w1", 2*i+1)
-		}
-		return nil
-	}
-
-	w2 := func(ctx context.Context) error {
-		for i := 0; i < 100; i++ {
-			time.Sleep(time.Millisecond)
-			fmt.Println("w2", 3*i+1)
-		}
-		return nil
-	}
-
-	task.Add(w1, w1, w1, func(ctx context.Context) error {
-		task.Add(w2, w2, w2, w2)
-		return nil
-	}, w1, w1)
-
-	actual := task.Run()
-
-	assert.Equal(t, nil, actual)
-
-	actual = task.Run()
-
-	assert.Equal(t, nil, actual)
+	err := tt.Run()
+	fmt.Println(err)
+	assert.Equal(t, context.DeadlineExceeded, err)
 }
